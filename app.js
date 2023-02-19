938c075988836e05f84bcb387dd4d879c4ab6854
danizgudrat <deniz.qudret16@mail.ru>
2023-02-19 16:06:40 +0400

    add

---
+++ app.js
@@ -0,0 +1,30 @@
+// let text = "Hello world !";
+// console.log(text.toLowerCase());
+// console.log(text.toUpperCase());
+// console.log(text[15]);
+// console.log(text.charAt(4));
+// console.log(text.indexOf("o"));
+// console.log(text.lastIndexOf("o"));
+// console.log(text.replace('l',"*"));
+// console.log(text.replaceAll('l',"*"));
+// console.log(text.split(" ")[2]);
+// console.log(text.substring(2,5));
+// console.log(text.slice(2,5));
+// console.log(text.substr(2,5));
+// let a = '${text}';
+// console.log(a);
+
+// let a = 4;
+
+// switch (a) {
+//     case 1:
+//         console.log("yanlis cavab");
+//         break;
+// case 2:
+//     console.log("yanlis cavab");
+//     break;
+//     case 4:
+//         console.log("dogru cavab");
+//     default:
+//         break;
+// }
